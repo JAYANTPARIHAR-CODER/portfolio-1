5a5898ca29f6d13e1b1051627b85e024ae1724b6
Delta Student <jayant1122.becse24@chitkara.edu.in>
2026-06-16 07:27:22 +0530

    Remove node_modules and dist from repository

--- dist/server/assets/_tanstack-start-manifest_v-Ds9vsp6H.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/JAYANT/Downloads/portfolio-code/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-D2-vZ7Q0.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-D2-vZ7Q0.js" } }] }, "/": { filePath: "C:/Users/JAYANT/Downloads/portfolio-code/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-Z2VJwXkH.js"] } } });
-export {
-  tsrStartManifest
-};
